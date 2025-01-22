f013b6b8e4d4db92caa0082b70d53a33c61007f8
Trwchus <zazmataz41@gmail.com>
2025-01-22 22:54:57 +0700

    Добавил меню, кнопку добавления события и подключил стили

--- App.js
+++ App.js
@@ -1,100 +1,282 @@
-import React, { useState, useEffect } from 'react'; // Импортируем необходимые хуки React
-import { View, Text, Button, Modal, TextInput } from 'react-native'; // Импортируем компоненты для UI из React Native
-import { Calendar } from 'react-native-calendars'; // Импортируем компонент календаря
-import * as Notifications from 'expo-notifications'; // Импортируем библиотеку для работы с уведомлениями
+import React, { useState } from 'react';
+import { View, Text, Button, Modal, TextInput, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import * as Notifications from 'expo-notifications';
+import LottieView from 'lottie-react-native';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Icon } from 'react-native-elements'; // Импортируем компонент Icon из React Native Elements
 
 export default function App() {
-  // Стейт для хранения выбранной даты
   const [selectedDate, setSelectedDate] = useState(null);
-  // Стейт для хранения событий, где ключом будет дата, а значением - название события
   const [events, setEvents] = useState({});
-  // Стейт для контроля видимости модального окна добавления события
   const [modalVisible, setModalVisible] = useState(false);
-  // Стейт для хранения названия события, которое пользователь вводит
   const [eventTitle, setEventTitle] = useState('');
+  const [eventTime, setEventTime] = useState(new Date());
+  const [editingEvent, setEditingEvent] = useState(null);
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [activeTab, setActiveTab] = useState('calendar'); // Стейт для активной вкладки
 
-  // Хук для обработки нажатия на день в календаре
   const handleDayPress = (day) => {
-    setSelectedDate(day.dateString); // Устанавливаем выбранную дату в стейт
-    if (events[day.dateString]) {
-      // Если на выбранную дату уже есть событие, показываем его
-      alert(`Event on this day: ${events[day.dateString]}`);
+    setSelectedDate(day.dateString);
+    if (events[day.dateString] && events[day.dateString].length > 0) {
+      setEditingEvent(null);
     } else {
-      // Если события на выбранный день нет, показываем модальное окно для ввода события
       setModalVisible(true);
     }
   };
 
-  // Хук для добавления события
   const handleAddEvent = async () => {
-    if (eventTitle && selectedDate) {
-      // Если название события и дата выбраны
-      const newEvents = { ...events, [selectedDate]: eventTitle }; // Добавляем новое событие в стейт
-      setEvents(newEvents); // Обновляем стейт событий
-      setModalVisible(false); // Закрываем модальное окно
-      setEventTitle(''); // Очищаем поле ввода
-
-      // Планируем уведомление на 9 утра в день события
+    // Если дата не выбрана, создаем событие на текущий день
+    const dateToUse = selectedDate || new Date().toISOString().split('T')[0];
+    if (eventTitle && dateToUse) {
+      const newEvent = { title: eventTitle, time: eventTime.toLocaleTimeString() };
+      const newEvents = { ...events };
+      
+      if (newEvents[dateToUse]) {
+        newEvents[dateToUse].push(newEvent);
+      } else {
+        newEvents[dateToUse] = [newEvent];
+      }
+      
+      setEvents(newEvents);
+      setModalVisible(false);
+      setEventTitle('');
+      setEventTime(new Date());
+      const notificationTime = new Date(`${dateToUse}T${eventTime.toLocaleTimeString()}`);
       await Notifications.scheduleNotificationAsync({
         content: {
-          title: 'Event Reminder', // Заголовок уведомления
-          body: `You have an event: ${eventTitle}`, // Текст уведомления
+          title: 'Напоминание о событии',
+          body: `У вас событие: ${eventTitle}`,
         },
-        trigger: { date: new Date(new Date(selectedDate).setHours(9, 0, 0, 0)) }, // Время триггера - 9:00 AM выбранной даты
+        trigger: { date: notificationTime },
       });
     }
   };
 
-  // Хук для удаления события
-  const handleDeleteEvent = () => {
+  const handleDeleteEvent = (eventIndex) => {
     if (selectedDate) {
-      // Если дата выбрана, удаляем событие для этой даты
       const newEvents = { ...events };
-      delete newEvents[selectedDate]; // Удаляем событие из объекта событий
-      setEvents(newEvents); // Обновляем стейт событий
+      newEvents[selectedDate].splice(eventIndex, 1);
+      if (newEvents[selectedDate].length === 0) {
+        delete newEvents[selectedDate];
+      }
+      setEvents(newEvents);
     }
   };
 
-  // Рендерим UI компоненты
-  return (
-    <View style={{ flex: 1, padding: 20 }}>
-      {/* Календарь, который позволяет выбрать дату */}
-      <Calendar
-        onDayPress={handleDayPress} // Обработчик события нажатия на день
-        markedDates={Object.keys(events).reduce((acc, date) => {
-          // Отображаем события, помечая выбранные даты
-          acc[date] = { selected: true, marked: true, selectedColor: 'blue' };
-          return acc;
-        }, {})}
-      />
+  const handleEditEvent = (eventIndex) => {
+    const eventToEdit = events[selectedDate][eventIndex];
+    setEventTitle(eventToEdit.title);
+    setEventTime(new Date(`${selectedDate}T${eventToEdit.time}`));
+    setEditingEvent(eventIndex);
+    setModalVisible(true);
+  };
 
-      {/* Модальное окно для добавления события */}
-      <Modal visible={modalVisible} animationType="slide" transparent={true}>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
-            {/* Поле ввода для названия события */}
-            <TextInput
-              placeholder="Enter event title"
-              value={eventTitle}
-              onChangeText={setEventTitle} // Обработчик изменения текста
-              style={{ borderBottomWidth: 1, marginBottom: 10, width: 200 }}
-            />
-            {/* Кнопка для добавления события */}
-            <Button title="Add Event" onPress={handleAddEvent} />
-            {/* Кнопка для закрытия модального окна */}
-            <Button title="Cancel" onPress={() => setModalVisible(false)} />
+  const showTimePickerHandler = () => {
+    setShowTimePicker(true);
+  };
+
+  const onTimeChange = (event, selectedDate) => {
+    setShowTimePicker(false);
+    if (selectedDate) {
+      setEventTime(selectedDate);
+    }
+  };
+
+  // Компоненты для вкладок
+  const renderCalendarTab = () => {
+    return (
+      <View style={styles.container}>
+        <Calendar
+          onDayPress={handleDayPress}
+          markedDates={Object.keys(events).reduce((acc, date) => {
+            acc[date] = { selected: true, marked: true, selectedColor: 'blue' };
+            return acc;
+          }, {})}
+          style={styles.calendar}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
+          <Ionicons name="add" size={40} color="white" /> {/* Иконка "+" для создания события */}
+        </TouchableOpacity>
+
+        {selectedDate && events[selectedDate] && events[selectedDate].length > 0 && (
+          <ScrollView style={styles.eventList}>
+            <Text style={styles.eventTitle}>События на {selectedDate}:</Text>
+            {events[selectedDate].map((event, index) => (
+              <View key={index} style={styles.eventItem}>
+                <Text>{event.title} в {event.time}</Text>
+                <View style={styles.iconButtons}>
+                  <Ionicons
+                    name="pencil"
+                    size={24}
+                    color="blue"
+                    onPress={() => handleEditEvent(index)}  // Карандаш вместо редактирования
+                  />
+                  <MaterialIcons
+                    name="delete"
+                    size={24}
+                    color="red"
+                    onPress={() => handleDeleteEvent(index)} // Кнопка удаления
+                  />
+                </View>
+              </View>
+            ))}
+          </ScrollView>
+        )}
+
+        <Modal visible={modalVisible} animationType="slide" transparent={true}>
+          <View style={styles.modalOverlay}>
+            <View style={styles.modalContent}>
+              <LottieView
+                source={require('./assets/animation.json')}  // Убедитесь, что путь к анимации правильный
+                autoPlay
+                loop
+                style={styles.lottie}
+              />
+              <TextInput
+                placeholder="Введите название события"
+                value={eventTitle}
+                onChangeText={setEventTitle}
+                style={styles.input}
+              />
+              <TouchableOpacity onPress={showTimePickerHandler}>
+                <Text style={styles.timeText}>Выберите время: {eventTime.toLocaleTimeString()}</Text>
+              </TouchableOpacity>
+
+              {showTimePicker && (
+                <DateTimePicker
+                  value={eventTime}
+                  mode="time"
+                  is24Hour={true}
+                  onChange={onTimeChange}
+                />
+              )}
+
+              <Button title={editingEvent !== null ? "Сохранить событие" : "Добавить событие"} onPress={handleAddEvent} />
+              <Button title="Отмена" onPress={() => setModalVisible(false)} />
+            </View>
           </View>
-        </View>
-      </Modal>
+        </Modal>
+      </View>
+    );
+  };
 
-      {/* Отображаем выбранную дату и возможность удалить событие для этой даты */}
-      {selectedDate && (
-        <View style={{ marginTop: 20 }}>
-          <Text>Selected Date: {selectedDate}</Text>
-          {/* Кнопка для удаления события */}
-          <Button title="Delete Event" onPress={handleDeleteEvent} />
-        </View>
-      )}
+  const renderProfileTab = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Имя пользователя: John Doe</Text>
+        <Text>Фото пользователя:</Text>
+        {/* В будущем сюда можно будет добавить фото пользователя */}
+      </View>
+    );
+  };
+
+  const renderFaqTab = () => {
+    return (
+      <View style={styles.container}>
+        <Text>FAQ - Полезные советы</Text>
+        {/* Заглушка, в будущем можно добавить информацию */}
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {activeTab === 'calendar' && renderCalendarTab()}
+      {activeTab === 'profile' && renderProfileTab()}
+      {activeTab === 'faq' && renderFaqTab()}
+
+      {/* Нижняя панель с навигацией */}
+      <View style={styles.bottomNav}>
+        <TouchableOpacity style={styles.navItem} onPress={() => setActiveTab('calendar')}>
+          <Icon name="calendar" type="font-awesome" size={30} color={activeTab === 'calendar' ? 'blue' : 'black'} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navItem} onPress={() => setActiveTab('profile')}>
+          <Icon name="user" type="font-awesome" size={30} color={activeTab === 'profile' ? 'blue' : 'black'} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navItem} onPress={() => setActiveTab('faq')}>
+          <Icon name="question-circle" type="font-awesome" size={30} color={activeTab === 'faq' ? 'blue' : 'black'} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 0, // Убираем лишние отступы со всего контейнера
+    backgroundColor: 'white',
+  },
+  calendar: {
+    marginTop: 20,
+  },
+  eventList: {
+    marginTop: 20,
+  },
+  eventTitle: {
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  eventItem: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  iconButtons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    width: 100,
+  },
+  addButton: {
+    marginTop: 20,
+    alignSelf: 'center',
+    backgroundColor: '#007BFF', // Синий фон для кнопки
+    padding: 15,
+    borderRadius: 50, // Круглая кнопка
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lottie: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    width: '100%',
+  },
+  timeText: {
+    fontSize: 18,
+    color: 'blue',
+    marginBottom: 10,
+  },
+  bottomNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+    backgroundColor: '#f0f0f0',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+    marginBottom: 0, // Убираем лишний отступ снизу
+  },
+  navItem: {
+    alignItems: 'center',
+    padding: 10, // Добавляем небольшой отступ для кнопок
+  },
+});
